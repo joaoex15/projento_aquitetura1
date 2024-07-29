f7b22d2a725daae0f5971448d988288975698df2
joaoex15 <joaosantoosrocha@gmail.com>
2024-07-29 12:16:40 -0300

    atualização 1

--- main.c
+++ main.c
@@ -8,10 +8,14 @@
 void classificacao(struct projeto *proj, int index) {
     char bin_opcode[7] = {0}; // Para armazenar o opcode em formato binário (6 bits + terminador nulo)
     char  bin_opcodeT[33] = {0};
+    char bin_x[5];
+    char bin_y[5];
+    char bin_z[5];
     uint32_t R[32] = { 0 };
 
     // Obtém os primeiros 6 bits da string binária
     strncpy(bin_opcode, proj->bins[index], 6);
+    
 
     // Obtém os primeiros 32 bits da string binária
     strncpy(bin_opcodeT, proj->bins[index], 32);
@@ -21,37 +25,47 @@ void classificacao(struct projeto *proj, int index) {
         snprintf(proj->clas[index], sizeof(proj->clas[index]), "operação ociosa");
     } else {
         if (strncmp(bin_opcode, "000000", 6) == 0) {
-            int x = proj->bins[index][6] - '0';
-            int z = proj->bins[index][7] - '0';
-            snprintf(proj->clas[index], sizeof(proj->clas[index]), "mov R[%d] = R[%d]", x, z);
+        
+            snprintf(proj->clas[index], sizeof(proj->clas[index]), "mov R[] = R[]" );
         }
         else if (strncmp(bin_opcode, "000001", 6) == 0) {
-            int x = proj->bins[index][6] - '0';
-            int z = proj->bins[index][7] - '0';
-            snprintf(proj->clas[index], sizeof(proj->clas[index]), "movs R[%d] = R[%d]", x, z);
+    
+            snprintf(proj->clas[index], sizeof(proj->clas[index]), "movs R[] = R[]" );
         }
 
 
     else if (strncmp(bin_opcode, "000010", 6) == 0) {
-            int x = proj->bins[index][6] - '0';
-            int z = proj->bins[index][7] - '0';
-            snprintf(proj->clas[index], sizeof(proj->clas[index]), "add R[%d] = R[%d]", x, z);
+        
+            snprintf(proj->clas[index], sizeof(proj->clas[index]), "add R[] = R[]" );
+            strncpy(bin_x,proj->bins[index] +  11, 5);
+            strncpy(bin_y,proj->bins[index] +  16, 5);
+            
+        }
+    else if (strncmp(bin_opcode, "000011", 6) == 0) {
+    
+            snprintf(proj->clas[index], sizeof(proj->clas[index]), "Sub R[] = R[]" );
+        }
+
+    else if (strncmp(bin_opcode, "000100", 6) == 0) {
+        // A varias operações similares com msm 6 digitos principais
+            snprintf(proj->clas[index], sizeof(proj->clas[index]), "mul R[] = R[]" );
+        }
+    else if (strncmp(bin_opcode, "000011", 6) == 0) {
+    
+            snprintf(proj->clas[index], sizeof(proj->clas[index]), "Cmp R[] = R[]" );
         }
 
 
 
 
         else if (strncmp(bin_opcode, "011000", 6) == 0) {
-            int x = proj->bins[index][6] - '0';
-            int z = proj->bins[index][7] - '0';
-            snprintf(proj->clas[index], sizeof(proj->clas[index]), "l8 R[%d] = MEM8[%d]", x, z);
+            snprintf(proj->clas[index], sizeof(proj->clas[index]), "l8 R[] = MEM8[]" );
         } else if (strncmp(bin_opcode, "011010", 6) == 0) {
-            int x = proj->bins[index][6] - '0';
-            int z = proj->bins[index][7] - '0';
-            snprintf(proj->clas[index], sizeof(proj->clas[index]), "l32 R[%d] = MEM32[%d]", x, z);
+
+            snprintf(proj->clas[index], sizeof(proj->clas[index]), "l32 R[] = MEM32[]" );
         } else if (strncmp(bin_opcode, "110111", 6) == 0) {
-            int x = proj->bins[index][6] - '0';
-            snprintf(proj->clas[index], sizeof(proj->clas[index]), "bun %d", x);
+    
+            snprintf(proj->clas[index], sizeof(proj->clas[index]), "bun " );
         } else if (strncmp(bin_opcode, "111111", 6) == 0) {
             snprintf(proj->clas[index], sizeof(proj->clas[index]), "int");
         } else {
@@ -69,7 +83,7 @@ int main() {
     struct projeto proj;  // Declaração de uma instância da struct projeto
     int index = 0;  // Índice para armazenar comandos no array hexcs
 
-    hex = fopen("/home/joao/Área de Trabalho/projentos-arquitetura/1_erro.hex", "r");
+    hex = fopen("/home/joao/Documentos/programas/ufs/projentos-arquitetura/1_erro.hex", "r");
     if (hex == NULL) {
         perror("Erro ao abrir o arquivo");
         return 1;
